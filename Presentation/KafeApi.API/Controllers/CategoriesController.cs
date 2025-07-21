00d32d8117f81bc51985ccfe4783364a5658e357
Murat Tokak <tokakmurat01@gmail.com>
2025-07-21 14:07:09 +0300

    Category Controller Oluşturma

---
+++ Presentation/KafeApi.API/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using KafeApi.Application.Dtos.CategoryDtos;
+using KafeApi.Application.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafeApi.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryServices _categoryServices;
+
+    public CategoriesController(ICategoryServices categoryServices)
+    {
+        _categoryServices = categoryServices;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+        var result = await _categoryServices.GetAllCategories();
+        return Ok(result);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdCategory(int id)
+    {
+        var result = await _categoryServices.GetByIdCategory(id);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCategory(CreateCategoryDto dto)
+    {
+        await _categoryServices.AddCategory(dto);
+        return Ok("Kategori Oluşturuldu.");
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateCategory(UpdateCategoryDto dto)
+    {
+        await _categoryServices.UpdateCategory(dto);
+        return Ok("Kategori güncellendi.");
+    }
+    [HttpDelete]
+    public async Task<IActionResult> DeleteCategory(int id)
+    {
+        await _categoryServices.DeleteCategory(id);
+        return Ok("Kategori silindi.");
+    }
+}
