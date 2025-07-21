bff92b1f1e3fc7348317ea590d702004e13c56cb
Murat Tokak <tokakmurat01@gmail.com>
2025-07-21 14:26:12 +0300

    Program.cs Yapılandırması

--- Presentation/KafeApi.API/Program.cs
+++ Presentation/KafeApi.API/Program.cs
@@ -1,13 +1,34 @@
+using KafeApi.Application.Interfaces;
+using KafeApi.Application.Mapping;
+using KafeApi.Application.Services.Abstract;
+using KafeApi.Application.Services.Concrete;
 using KafeApi.Persistence.Context;
+using KafeApi.Persistence.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("KafeDbContext")))
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    var conf = builder.Configuration.GetConnectionString("DefaultConnection");
+    opt.UseSqlServer(conf);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IMenuItemServices, MenuItemServices>();
+builder.Services.AddScoped<ICategoryServices, CategoryServices>();
+
+
+builder.Services.AddAutoMapper(typeof(GeneralMapping));
+
+
+
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
